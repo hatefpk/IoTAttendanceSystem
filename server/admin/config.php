3a548f26c957b00a0906dc77748ddf438a2c5c45
hatefpk <hatefsafaripk@gmail.com>
2025-07-29 16:50:35 +0330

    Update config.php
    fix config

--- server/admin/config.php
+++ server/admin/config.php
@@ -9,7 +9,6 @@ define("DB_NAME", "attendance_db");
 // Manually maintained list of Pico IPs
 $pico_ips = [
     "192.168.1.101", // Pico 1
-    "192.168.1.102", // Pico 2
     // Add more as needed
 ];
 ?>
